de1439460d75bf144d71b12004294518845e8d32
SergeyShumeev <print.agwe@gmail.com>
2021-02-24 18:45:43 +0200

    Revert "Revert "Add Task1 solution""
    This reverts commit 4f4db87c

--- src/main/java/day2/Task4.java
+++ src/main/java/day2/Task4.java
@@ -10,7 +10,7 @@ public class Task4 {
 
         if (x <= -3) System.out.println(420);
         else {
-            if ((x < -3) && (x > 5))    System.out.println((x + 3) * (Math.pow(x, 2) - 2));
+            if ((x < -3) && (x > 5))     System.out.println((x + 3) * (Math.pow(x, 2) - 2));
             if (x >= 5)                  System.out.println((x*x - 10) / (100 + 7));
         }
                     // Обошёлся без переменной y
