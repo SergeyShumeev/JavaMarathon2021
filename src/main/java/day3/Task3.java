023d56b1675ec2617bfd890a5ff9be1e58472b1f
SergeyShumeev <print.agwe@gmail.com>
2021-02-24 18:16:34 +0200

    Revert "Revert "Add Task1 solution""
    This reverts commit 4f4db87c

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,27 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        double dividend;
+        double divider;
+        int i = 0;
+
+        while (i < 5){
+
+            dividend = scanner.nextInt();
+            divider = scanner.nextInt();
+
+            if (divider == 0) {
+                    System.out.println("Деление на 0");
+                    i--;
+                }
+            if (divider != 0) System.out.println(dividend / divider);
+
+            i++;
+        }
     }
 }
